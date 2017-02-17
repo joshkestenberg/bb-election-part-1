754055127859f351a69a4121ce94ce1e5a92a152
joshkestenberg <joshkestenberg@gmail.com>
2017-02-17 17:03:42 -0500

    assign complete

--- election.js
+++ election.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
 
-  // Imagination!
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    datatype: 'json'
+  }).done(function(data){
+    cands = data.candidates;
+    $.each(cands, function(){
+      $('<li>').html('name: ' + this.name + '; votes: ' + this.votes).appendTo('#candidates');
+    });
+  });
 
 });
